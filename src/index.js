35bddd8db690f5c072c0406fef57965e73322767
ankitchoudhary8120@gmail.com <ankitchoudhary8120@gmail.com>
2023-09-24 16:33:35 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux'; // Import createStore from Redux
+
+// Define a reducer function (you should replace this with your actual reducer)
+const rootReducer = (state, action) => {
+  // Your reducer logic here
+  return state;
+};
+
+// Create the Redux store
+const store = createStore(rootReducer);
+
+store.subscribe(() => console.log(store.getState()));
+
+const root = document.getElementById('root'); // No need to use ReactDOM.createRoot here
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  root
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
